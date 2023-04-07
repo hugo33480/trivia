c35ab6f061345c3b013bba521e678ca46473d8e7
Théo Delas <theo.delas@gmail.com>
2023-04-07 16:40:56 +0200

    feat: fix the displaying round in the log

--- typescript/src/game-runner.ts
+++ typescript/src/game-runner.ts
@@ -6,16 +6,22 @@ export class GameRunner {
   public static main(game: Game): void {
     let notAWinner;
     let nb_round = 1;
+    let nb_players = game.players.length
+    let how_many_players_have_play_this_round = 0
     if (game.isNumberOfPlayerValid() && game.isCoinGoalValid()) {
       do {
+        how_many_players_have_play_this_round += 1
         game.roll(Math.floor(Math.random() * 6) + 1, nb_round);
-        nb_round += 1
         if (game.giveUp()) {
           notAWinner = game.didPlayerWin();
         } else if (!game.players[game.currentPlayer].alwaysTrueAnswer && Math.floor(Math.random() * 10) == 7 || game.players[game.currentPlayer].alwaysFalseAnswer) {
           notAWinner = game.wrongAnswer();
         } else {
           notAWinner = game.wasCorrectlyAnswered();
+        }
+        if( how_many_players_have_play_this_round === nb_players) {
+          how_many_players_have_play_this_round = 0
+          nb_round += 1
         }
       } while (notAWinner);
     }
